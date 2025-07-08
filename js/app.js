8d1cac7236d97c0d7f7eb1b78138b960c8fea13b
nelicepriatniy <clavik.polyakov@yandex.ru>
2025-07-08 20:22:03 +0300

    страница Практики и курсы

--- js/app.js
+++ js/app.js
@@ -7,3 +7,22 @@ const swiper = new Swiper('.nagradi-slider', {
     prevEl: '.nagrady-arrow.prev'
   }
 });
+
+
+//курсы 
+
+const curses = document.querySelectorAll('.main-curses .item')
+
+if(curses.length > 0) {
+  curses.forEach((el)=>{
+    const openBtn = el.querySelector('.top .more')
+    const closeBtn = el.querySelector('.bottom .back')
+    openBtn.onclick = ()=>{
+      el.classList.add('active')
+    }
+    closeBtn.onclick = ()=>{
+      el.classList.remove('active')
+    }
+  })
+}
+

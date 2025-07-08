895175ee7064a94a95e2e98b37271c9d4e686cb4
nelicepriatniy <clavik.polyakov@yandex.ru>
2025-07-08 14:38:25 +0300

    создание страницы о мастере десктоп

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.nagradi-slider', {
+  speed: 400,
+  spaceBetween: 70,
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.nagrady-arrow.next',
+    prevEl: '.nagrady-arrow.prev'
+  }
+});
